98afd335e1664a433880ab78182670a7b3951260
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-15 10:29:40 +0300

    урок 16

--- script.js
+++ script.js
@@ -1,70 +1,20 @@
 'use strict';
-//Функция конструктор
-let DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-};
 
-//Метод конструктора
-DomElement.prototype.createElement = function () {
-  let ferstSymbol = this.selector[0];
-  let nextSymbol = this.selector.substring(1);
+class  First{
+   hello(){
+     console.log("Привет я метод родителя!");
+   }
+}
 
-  if (ferstSymbol == ".") {
-    let div = document.createElement('div');
-    div.classList.add = nextSymbol;
-    div.style.cssText = 
-    `height:` + this.height + `px;
-    width:` + this.width + `px;
-    background-color:` + this.bg + `;
-    font-size:` + this.fontSize + `px;
-    position: absolute;`;
-    div.innerHTML = '';
-    document.body.append(div);
-  } else if (ferstSymbol == "#"){
-    let p = document.createElement('p');
-    p.id = nextSymbol;
-    p.style.cssText = 
-    `height:` + this.height + `px;
-    width:` + this.width + `px;
-    background-color:` + this.bg + `;
-    font-size:` + this.fontSize + `px;
-    position: absolute;`;
-    p.innerHTML = this.selector;
-    document.body.append(p);
-  } else{
-    alert("Вы ввели не selector");
+let newFirst = new First();
+
+class Second extends First{
+  hello(){
+    newFirst.hello();
+    console.log("А я наследуемый метод!");
   }
-};
+}
 
-let str = prompt("Введите selector", ".example");
+let newSecond = new Second();
 
-//Создание объекта через конструктор
-let person = new DomElement(str, '100', '100', 'yellow', '20');
-
-//Вызов метода
-person.createElement();
-
-
-
-let sq = document.querySelector('div');
-document.addEventListener('keydown', function(event){
-
-  if (event.code == 'ArrowRight'){
-    sq.style.right = parseFloat(sq.style.right || 0) - 10 + 'px';
-  }
-  if (event.code == 'ArrowLeft'){
-    sq.style.left = parseFloat(sq.style.left || 0) - 10 + 'px';
-  }
-  if (event.code == 'ArrowUp'){
-    sq.style.top = parseFloat(sq.style.top || 0) - 10 + 'px';
-  }
-  if (event.code == 'ArrowDown'){
-    sq.style.bottom = parseFloat(sq.style.bottom || 0) - 10 + 'px';
-  }
-});
-
-
+newSecond.hello();
